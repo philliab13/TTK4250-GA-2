6ef917d7817bc8cd19e50110cfd3d8a61a6f73fc
philliab13 <philliab@stud.ntnu.no>
2025-10-28 16:31:02 +0100

    Oppg d) finished

--- group_assignment2/src/EKFSLAM.py
+++ group_assignment2/src/EKFSLAM.py
@@ -78,11 +78,9 @@ class EKFSLAM:
         np.ndarray
             The Jacobian of f wrt. u.
         """
-        # TODO replace this with your own code
-        Fu = solution.EKFSLAM.EKFSLAM.Fu(self, x, u)
-        return Fu
-
-        Fu = None  # TODO, eq (11.14)
+        heading = utils.wrapToPi(x[2])
+        Fu = np.array([[np.cos(heading), -np.sin(heading), 0],
+                      [np.sin(heading), np.cos(heading), 0], [0, 0, 1]])  # TODO, eq (11.14)
 
         return Fu
 
@@ -105,8 +103,8 @@ class EKFSLAM:
         Tuple[np.ndarray, np.ndarray], shapes= (3 + 2*#landmarks,), (3 + 2*#landmarks,)*2
             predicted mean and covariance of eta.
         """
-        etapred, P = solution.EKFSLAM.EKFSLAM.predict(self, eta, P, z_odo)
-        return etapred, P
+
+        # return etapred, P
 
         # check inout matrix
         assert np.allclose(P, P.T), "EKFSLAM.predict: not symmetric P input"
@@ -119,20 +117,23 @@ class EKFSLAM:
         etapred = np.empty_like(eta)
 
         x = eta[:3]
-        etapred[:3] = None  # TODO robot state prediction
-        etapred[3:] = None  # TODO landmarks: no effect
+        # TODO robot state prediction (11.7)
+        etapred[:3] = self.f(x, z_odo)
+        etapred[3:] = eta[3:]  # TODO landmarks: no effect
 
-        Fx = None  # TODO
-        Fu = None  # TODO
+        Fx = self.Fx(x, z_odo)  # TODO
+        Fu = self.Fu(x, z_odo)  # TODO
 
         # evaluate covariance prediction in place to save computation
         # only robot state changes, so only rows and colums of robot state needs changing
         # cov matrix layout:
         # [[P_xx, P_xm],
         # [P_mx, P_mm]]
-        P[:3, :3] = None  # TODO robot cov prediction
-        P[:3, 3:] = None  # TODO robot-map covariance prediction
-        P[3:, :3] = None  # TODO map-robot covariance: transpose of the above
+        P[:3, :3] = Fx@P[:3, :3]@Fx.T + \
+            Fu@self.Q@Fu.T  # TODO robot cov prediction Fu siden vi ikke har perfekt måling, må ha med noise
+        P[:3, 3:] = Fx@P[:3, 3:]  # TODO robot-map covariance prediction
+        # TODO map-robot covariance: transpose of the above
+        P[3:, :3] = P[:3, 3:].T
 
         assert np.allclose(P, P.T), "EKFSLAM.predict: not symmetric P"
         assert np.all(
@@ -157,10 +158,6 @@ class EKFSLAM:
         np.ndarray, shape=(2 * #landmarks,)
             The landmarks in the sensor frame.
         """
-
-        # TODO replace this with your own code
-        zpred = solution.EKFSLAM.EKFSLAM.h(self, eta)
-        return zpred
 
         # extract states and map
         x = eta[0:3]
